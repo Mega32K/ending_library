50b9decc7b89ff4a75de78941b147932087c96a1
Mega32K <1830877341@qq.com>
2025-11-17 04:38:14 +0800

    撤销custom_组件#2

--- src/main/java/com/mega/endinglib/mixin/advanced/data_expand/component/entity_data/ArrowMixin.java
+++ src/main/java/com/mega/endinglib/mixin/advanced/data_expand/component/entity_data/ArrowMixin.java
@@ -1,10 +1,9 @@
-package com.mega.endinglib.mixin.advanced.data_expand.component.custom_data;
+package com.mega.endinglib.mixin.advanced.data_expand.component.entity_data;
 
 import com.mega.endinglib.api.item.component.DataComponents;
 import com.mega.endinglib.api.item.component.ItemComponentManager;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.commands.data.DataCommands;
 import net.minecraft.server.commands.data.EntityDataAccessor;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.AbstractArrow;
@@ -22,18 +21,5 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class ArrowMixin extends AbstractArrow {
     ArrowMixin(EntityType<? extends AbstractArrow> p_36721_, Level p_36722_) {
         super(p_36721_, p_36722_);
-    }
-
-    @Shadow public abstract void addAdditionalSaveData(@NotNull CompoundTag p_36881_);
-
-    @Inject(method = "setEffectsFromItem", at = @At("HEAD"))
-    private void setCustomDataFromItem(ItemStack itemStack, CallbackInfo ci) {
-        CompoundTag tag = ItemComponentManager.get(itemStack, DataComponents.CUSTOM_DATA);
-        if (tag != null) {
-            EntityDataAccessor dataAccessor = new EntityDataAccessor(this);
-            try {
-                dataAccessor.setData(dataAccessor.getData().merge(tag));
-            } catch (CommandSyntaxException ignore) {}
-        }
-    }
+    } 
 }
