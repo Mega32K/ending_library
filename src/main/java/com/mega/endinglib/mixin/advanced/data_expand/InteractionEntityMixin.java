7345267013729a8a778aa751a2cda02d9484def6
Mega32K <1830877341@qq.com>
2025-11-18 03:24:05 +0800

    EL为交互实体调用的命令不再发布消息

--- src/main/java/com/mega/endinglib/mixin/advanced/data_expand/InteractionEntityMixin.java
+++ src/main/java/com/mega/endinglib/mixin/advanced/data_expand/InteractionEntityMixin.java
@@ -1,8 +1,15 @@
 package com.mega.endinglib.mixin.advanced.data_expand;
 
 import com.mega.endinglib.api.data.CompoundTagUtils;
+import net.minecraft.commands.CommandSigningContext;
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.arguments.EntityAnchorArgument;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.TaskChainer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
@@ -10,12 +17,16 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Interaction;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import javax.annotation.Nullable;
 
 @Mixin(Interaction.class)
 public abstract class InteractionEntityMixin extends Entity {
@@ -55,20 +66,32 @@ public abstract class InteractionEntityMixin extends Entity {
     private void onInteract(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
         if (this.level() instanceof ServerLevel sl) {
             if (this.interactionCommand != null && !this.interactionCommand.isEmpty())
-                sl.getServer().getCommands().performPrefixedCommand(this.createCommandSourceStack().withMaximumPermission(2), this.interactionCommand);
+                sl.getServer().getCommands().performPrefixedCommand(endingLibrary$customCSS(sl), this.interactionCommand);
         }
     }
     @Inject(method = "skipAttackInteraction", at = @At(value = "RETURN", ordinal = 0, shift = At.Shift.BEFORE))
-    private void attackCommand(Entity entity, CallbackInfoReturnable<Boolean> cir) {if (this.level() instanceof ServerLevel sl) {
-        if (this.attackCommand != null && !this.attackCommand.isEmpty())
-            sl.getServer().getCommands().performPrefixedCommand(this.createCommandSourceStack().withMaximumPermission(2), this.attackCommand);
-    }
+    private void attackCommand(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if (this.level() instanceof ServerLevel sl) {
+            if (this.attackCommand != null && !this.attackCommand.isEmpty()) {
+                sl.getServer().getCommands().performPrefixedCommand(endingLibrary$customCSS(sl).withMaximumPermission(2), this.attackCommand);
+            }
+        }
     }
     @Inject(method = "tick", at = @At("HEAD"))
     private void tickCommand(CallbackInfo ci) {
         if (this.level() instanceof ServerLevel sl) {
             if (this.tickingCommand != null && !this.tickingCommand.isEmpty())
-                sl.getServer().getCommands().performPrefixedCommand(this.createCommandSourceStack().withMaximumPermission(2), this.tickingCommand);
+                sl.getServer().getCommands().performPrefixedCommand(endingLibrary$customCSS(sl), this.tickingCommand);
         }
+    }
+    @Unique
+    private CommandSourceStack endingLibrary$customCSS(ServerLevel serverLevel) {
+        return endingLibrary$newCSS(this, this.position(), this.getRotationVector(), serverLevel, Math.max(this.getPermissionLevel(), 2), this.getName().getString(), this.getDisplayName(), serverLevel.getServer(), this);
+    }
+    @Unique
+    private static CommandSourceStack endingLibrary$newCSS(CommandSource p_81302_, Vec3 p_81303_, Vec2 p_81304_, ServerLevel p_81305_, int p_81306_, String p_81307_, Component p_81308_, MinecraftServer p_81309_, @Nullable Entity p_81310_) {
+        return new CommandSourceStack(p_81302_, p_81303_, p_81304_, p_81305_, p_81306_, p_81307_, p_81308_, p_81309_, p_81310_, true, (p_81361_, p_81362_, p_81363_) -> {
+        }, EntityAnchorArgument.Anchor.FEET, CommandSigningContext.ANONYMOUS, TaskChainer.immediate(p_81309_), (p_280930_) -> {
+        });
     }
 }
