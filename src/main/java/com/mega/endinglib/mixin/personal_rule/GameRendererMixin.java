f435e13bfceb1559ad77ab9af2c40351e50875bd
Mega32K <1830877341@qq.com>
2026-02-22 18:09:59 +0800

    修复移动视角和受伤视角可能的崩溃风险

--- src/main/java/com/mega/endinglib/mixin/personal_rule/GameRendererMixin.java
+++ src/main/java/com/mega/endinglib/mixin/personal_rule/GameRendererMixin.java
@@ -3,9 +3,12 @@ package com.mega.endinglib.mixin.personal_rule;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mega.endinglib.client.ClientWrapped;
 import com.mega.endinglib.proxy.CommonProxy;
+import com.mega.endinglib.util.java.MUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.world.entity.player.Player;
+import org.apache.commons.lang3.mutable.MutableFloat;
+import org.apache.commons.lang3.mutable.MutableObject;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,19 +24,25 @@ public abstract class GameRendererMixin {
 
     @ModifyExpressionValue(method = "bobView", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;lerp(FFF)F", ordinal = 0))
     private float modifyMultiplierOfWalkingView(float original) {
+        MutableFloat f = new MutableFloat(original);
         Player player = ClientWrapped.clientPlayer();
         if (player != null && player.isAlive()) {
-            return original * CommonProxy.getCameraCap(player).getWalkingViewMultiplier();
+            CommonProxy.getCameraCapOptional(player).ifPresent(cap -> {
+                f.setValue(original * CommonProxy.getCameraCap(player).getWalkingViewMultiplier());
+            });
         }
-        return original;
+        return f.getValue();
     }
 
     @ModifyExpressionValue(method = "bobHurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/OptionInstance;get()Ljava/lang/Object;", ordinal = 0))
     private <T> T modifyMultiplierOfHurtView(T original) {
+        MutableObject<T> object = new MutableObject<>(original);
         Player player = ClientWrapped.clientPlayer();
         if (player != null && original instanceof Double d && player.isAlive()) {
-            return (T) (Object) (d * CommonProxy.getCameraCap(player).getHurtViewMultiplier());
+            CommonProxy.getCameraCapOptional(player).ifPresent(cap -> {
+                object.setValue(MUtils.objectForced((double) (d * CommonProxy.getCameraCap(player).getHurtViewMultiplier())));
+            });
         }
-        return original;
+        return object.getValue();
     }
 }
